f08c67ba39b416c2f56680bde6c4ef91fd443b1e
이수민 <min001017@naver.com>
2021-05-11 13:14:21 +0900

    insufficient에외 출력 수정

--- travel_application/exception/InsufficientConditionException.java
+++ travel_application/exception/InsufficientConditionException.java
@@ -2,7 +2,7 @@ package assignment2.exception;
 
 public class InsufficientConditionException extends Exception {
 	public InsufficientConditionException() {
-		super("insufficient condition");
+		super("**INSUFFICIENT CONDITION: please select again.**");
 	}
 	public InsufficientConditionException(String message) {
 		super(message);
